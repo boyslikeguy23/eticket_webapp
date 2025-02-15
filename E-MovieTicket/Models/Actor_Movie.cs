c09c6ccbc99e21a369edb9eec0044ee80e0e3503
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-02-15 21:09:54 +0700

    add public Actor

--- E-MovieTicket/Models/Actor_Movie.cs
+++ E-MovieTicket/Models/Actor_Movie.cs
@@ -1,8 +1,12 @@
-﻿namespace E_MovieTicket.Models;
+﻿using eTickets.Models;
+
+namespace E_MovieTicket.Models;
 
 public class Actor_Movie
 {
     public int MovieId { get; set; }
     public Movie Movie { get; set; }
     public int ActorId { get; set; }
+    public Actor Actor { get; set; }
+
 }
