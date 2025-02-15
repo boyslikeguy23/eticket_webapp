3598c21250c1e7bfd9f78135ccbec9e2e3f800c3
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-02-16 00:05:45 +0700

    add ViewModels

---
+++ E-MovieTicket/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,24 @@
+﻿using E_MovieTicket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using E_MovieTicket.Models;
+using eTickets.Models;
+
+namespace eTickets.Data.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+}
---
+++ E-MovieTicket/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,9 @@
+﻿using E_MovieTicket.Data.Cart;
+
+namespace E_MovieTicket.Data.ViewModels;
+
+public class ShoppingCartVM
+{
+    public ShoppingCart ShoppingCart { get; set; }
+    public double ShoppingCartTotal { get; set; }
+}
