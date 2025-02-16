f4c2df249be17a9fd7d1c02f2b1e4c72c7cd0169
boyslikeguy23 <tungxuanmai2003@gmail.com>
2025-02-16 19:46:14 +0700

    add Controller modules

--- E-MovieTicket/Models/Order.cs
+++ E-MovieTicket/Models/Order.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace E_MovieTicket.Models;
 
 public class Order
 {
+    [Key]
     public int Id { get; set; }
     public string Email { get; set; }
     public string UserId {get; set;}
     [ForeignKey(nameof(UserId))]
     public ApplicationUser User {get; set;}
-    public List<OrderItem> Items { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
 }
